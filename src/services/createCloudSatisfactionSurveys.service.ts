7d6896489de80cc180034c3886b10a6bfdf227c9
gabrielmendezsoares <gabrielmendezsoares@gmail.com>
2025-08-26 10:38:04 -0300

    fix: schemas

--- src/services/createCloudSatisfactionSurveys.service.ts
+++ src/services/createCloudSatisfactionSurveys.service.ts
@@ -1,5 +1,5 @@
 import momentTimezone from 'moment-timezone';
-import { sigma_cloud_neppo_integration_satisfaction_surveys, PrismaClient } from '@prisma/client/storage/client.js';
+import { PrismaClient, sigma_cloud_neppo_integration_satisfaction_surveys } from '@prisma/client/storage/client.js';
 import { HttpClientUtil, loggerUtil, BearerStrategy } from '../../expressium/index.js';
 import { normalizePhoneService } from './index.js';
 import { IActivity, ICloudServiceOrder, ICloudServiceOrderFromId, ICloudServiceOrderList, IUserAndContact } from './interfaces/index.js';
